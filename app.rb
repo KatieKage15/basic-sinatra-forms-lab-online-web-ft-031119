7038af5d1ad50475ece74a9156948b43a25eb260
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-26 15:49:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get "/team" do
+    erb :"/team/index.html"
+  end
 
+  get "/team/new" do
+    erb :"/team/new.html"
+  end
+
+  get "/team" do
+    erb :team
+  end
+
+  post "/team" do
+    team = Team.new(params)
+    team.save
+    redirect "/workouts/#{workout.id}"
+  end
 end
